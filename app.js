c4b84cc60a018b452cd000c45493a7632bc03b93
ashik-210 <ashikidrishi210@gmail.com>
2026-06-26 10:59:52 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+let turn0 = true;
+let count = 0;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const showDraw = () => {
+    msg.innerText = "Game Draw!";
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const resetGame =() =>{
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    count = 0;
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turn0){
+            box.innerText="O";
+            box.style.color = "blue"; 
+            turn0=false;
+        }
+        else{
+            box.innerText="X";
+            box.style.color = "red";  
+            turn0=true;
+        }
+        box.disabled = true;
+        count++;
+        checkWinner();
+    })
+})
+
+const disableBoxes = () =>{
+    for (let box of boxes) {
+        box.disabled = true;
+
+        
+    }
+}
+const enableBoxes = () =>{
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText="";
+        
+    }
+}
+
+const showWinner = (winner) =>{
+    msg.innerText=`Congratulations, winner is ${winner}`
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+} 
+
+const checkWinner = () =>{
+    let winnerfound = false;
+
+    for (pattern of winPatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val !="" && pos2val!="" && pos3val !=""){
+            if(pos1val==pos2val && pos2val==pos3val){
+                console.log("winner", pos1val);
+                winnerfound = true;
+                showWinner(pos1val);
+
+            }
+        }
+    }
+    if(count === 9 && !winnerfound){
+        showDraw();
+    }
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetbtn.addEventListener("click",resetGame);
+
